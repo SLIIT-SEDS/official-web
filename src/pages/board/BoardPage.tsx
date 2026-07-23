fff0f829a57f82bc4030a8ff01d14dbe32c5e45b
MrVirul <virul.mmeemana@gmail.com>
2026-07-23 22:08:13 +0530

    feat(routes): add BoardPage and register /board route in App

---
+++ src/pages/board/BoardPage.tsx
@@ -0,0 +1,11 @@
+import BoardMembersSection from '@/features/board/components/BoardMembersSection';
+
+const BoardPage = () => {
+  return (
+    <div className="min-h-screen bg-[#0a0a0e]">
+      <BoardMembersSection />
+    </div>
+  );
+};
+
+export default BoardPage;
